db4efcd9c311ff1b792a0bfe6a88b29fd38810ad
Rotem Tzaban <rotemtzaban@gmail.com>
2019-10-17 17:38:31 +0300

    webworker works but shit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import Python from './python.worker.js';
+
 declare var languagePluginLoader: any;
 declare var pyodide: any;
-const App: React.FC = () => {
-  languagePluginLoader.then(async () => {
-    // pyodide is now ready to use...
-    console.log("init done");
-    console.log(pyodide.runPython("import sys\nsys.version"));
-    console.log(pyodide.runPython('[4, 5, "uasd"]'));
-    await pyodide.loadPackage(["numpy"]);
-    console.log(pyodide.runPython("import numpy as np"));
-    const work = await fetch("http://localhost:5000/work");
-    const workText = await work.text();
-    print(workText);
-  });
 
-  function print(str: string) {
-    console.log(pyodide.runPython(str));
+const App: React.FC = () => {
+  const pyodideWorker = new Worker('./python.worker.js');
+  pyodideWorker.onmessage = (e) => {
+    const {results, error} = e.data
+    if (results) {
+      console.log('pyodideWorker return results: ', results)
+    } else if (error) {
+      console.log('pyodideWorker error: ', error)
+    }
   }
+  
+  const data = {
+    A_rank: [0.8, 0.4, 1.2, 3.7, 2.6, 5.8],
+    python:
+      'import statistics\n' +
+      'from js import A_rank\n' +
+      'statistics.stdev(A_rank)'
+  }
+  
+  pyodideWorker.postMessage(data)
+  // languagePluginLoader.then(async () => {
+  //   // pyodide is now ready to use...
+  //   console.log("init done");
+  //   console.log(pyodide.runPython("import sys\nsys.version"));
+  //   console.log(pyodide.runPython('[4, 5, "uasd"]'));
+  //   await pyodide.loadPackage(["numpy"]);
+  //   console.log(pyodide.runPython("import numpy as np"));
+  //   const work = await fetch("http://localhost:5000/work");
+  //   const workText = await work.text();
+  //   print(workText);
+  // });
+
+  // function print(str: string) {
+  //   console.log(pyodide.runPython(str));
+  // }
   return (
     <div className="App">
       <header className="App-header">
