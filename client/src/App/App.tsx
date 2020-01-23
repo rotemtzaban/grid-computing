208af99276e480b50e58c7c5ad81ea1a9b7e2284
Ido Bloch <idoprv@gmail.com>
2020-01-23 09:25:49 +0200

    start the upload page

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Upload from "../Upload/Upload";
 import logo from "../logo.svg";
 import "./App.css";
 /* eslint import/no-webpack-loader-syntax: off */
@@ -34,21 +35,16 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {/* <Compute msg="kof"></Compute> */}
+                <Upload></Upload>
             </header>
         </div>
     );
+    
+
+    
 };
+
+
 
 export default App;
