354f14f152e4f7d9a6c6d797b1538c88977d16f9
Ido Bloch <idoprv@gmail.com>
2020-02-07 18:49:27 +0200

    added upload route to upload data&code files

--- server/main.py
+++ server/main.py
@@ -1,8 +1,19 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, flash, redirect, url_for
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
+#cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
 
 global worker_num
 global total_workers_num
@@ -12,6 +23,31 @@ global results
 @app.route('/test')
 def hello_world():
     return 'Hello, World!'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+# @cross_origin()
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return "test"
 
 
 @app.route('/work')
@@ -47,11 +83,17 @@ def finished():
     if total_workers_num == worker_num:
         f = open("example.py", "r")
         exec(f.read() + '\nglobal reduced_value\nreduced_value = reduce(' + str(results) + ')')
+        print('total result = ')
         print(reduced_value)
     else:
         print('Waiting for more...')
 
     return 'Thanks, peretz is stright'
+
+# @socket.on('connect')
+# def on_connect():
+#     print("peretz is gay")
+#     emit('work', 'import numpy as np\nnp.array([1, 3, 5])\nnp.version.version')
 
 
 if __name__ == "__main__":
